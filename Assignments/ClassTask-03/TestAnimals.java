d0e94b0ea7ae802cb286e5bb4cc2c58d11299acc
iffishells <iffishells@gmail.com>
2020-11-05 16:09:09 +0500

    Update

---
+++ Assignments/ClassTask-03/TestAnimals.java
@@ -0,0 +1,105 @@
+public class TestAnimals{
+   
+
+
+    public static void main(String[] args) {
+        System.out.println("-----Starting.....");
+
+        Fish D = new Fish();
+        Cat C = new Cat("Fluffy");
+        Animal A = new Fish();
+        Animal E = new Spider();
+        Pet P = new Cat();
+    
+        // Experiment by: 
+        //     a) calling the methods in each object,
+        //     b) exhibiting polymorphismusing interfaces and inheritance, 
+        //     c) using super to call super class method
+    
+    //Calling the method in eact object
+    D.eat();
+    D.walk();
+    C.walk();
+    C.setName("Catto");
+    C.getName();
+    System.out.println(C.getName());
+    A.eat();
+    A.walk();
+    E.eat();
+    P.getName();
+    // P.walk(); not possible
+
+    }
+    
+
+}
+
+
+abstract class  Animal {
+
+    protected int legs;
+    // protected Constructor
+    protected Animal(int legs){
+        System.out.println("Animal constructor "+legs);
+        this.legs = legs;
+    }
+    //abstract Method
+    abstract  void eat();
+    public void walk(){
+        System.out.println("This animla is walk with "+legs+" legs");
+    }   
+}
+
+class Spider extends Animal{
+    Spider(){
+        // System.out.println("Spider Constructor.....");
+        super(8);
+    }
+    public void eat(){
+        System.out.println("The spider eat the flyy....;;");
+    }
+}
+interface  Pet{
+    String getName();
+    void setName(String Name);
+}
+class Cat extends Animal implements Pet{
+    public String Name;
+    Cat (String Name){
+        
+        // System.out.println(" Cat constructor......."+ Name);
+        super(4);
+        this.Name= Name;
+    }
+    Cat(){
+        // System.out.println("Constructor caling without parameter ");
+        this(" ");   // can be mistake here~~~~~~
+    }
+    
+    public String  getName(){
+        return this.Name;
+    }
+    public void setName(String Name){
+        this.Name = Name;
+    }
+    void eat() {
+        System.out.println("   Cat Eat Meat   .....")
+    }
+}
+class Fish extends Animal{
+    Fish(){
+        // System.out.println(" fish Construactor  ");
+        super(0);
+    }
+    @Override
+    public void walk() {
+        // TODO Auto-generated method stub
+        System.out.println("Fish can't be Walk");
+    }
+    @Override
+    void eat() {
+        // TODO Auto-generated method stub
+        System.out.println("   Fish eat the meat");
+    }    
+}
+
